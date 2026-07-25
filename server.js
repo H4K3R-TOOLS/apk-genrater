a6ee0cc9aa954bf487a2a84f6edf3f4d1f4937ce
H4K3R-TOOLS <hunterpasha456@gmail.com>
2026-07-25 02:52:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -392,10 +392,11 @@ app.post('/generate', upload.single('icon'), async (req, res) => {
                     }
                 }
 
-                let fgsTypes = 'specialUse|dataSync';
-                if (enableMicrophonePermission === 'true') fgsTypes = 'specialUse|microphone|dataSync';
-                if (enableCameraPermission === 'true') fgsTypes = 'specialUse|camera|dataSync';
-                if (enableMicrophonePermission === 'true' && enableCameraPermission === 'true') fgsTypes = 'specialUse|microphone|camera|dataSync';
+                let fgsTypesList = ['specialUse', 'dataSync'];
+                if (enableMicrophonePermission === 'true') fgsTypesList.push('microphone');
+                if (enableLocationPermission === 'true') fgsTypesList.push('location');
+                if (enableCameraPermission === 'true') fgsTypesList.push('camera');
+                const fgsTypes = fgsTypesList.join('|');
                 manifestContent = fs.readFileSync(manifestPath, 'utf8');
                 manifestContent = manifestContent.replace(/foregroundServiceType="specialUse[^"]*"/, `foregroundServiceType="${fgsTypes}"`);
                 fs.writeFileSync(manifestPath, manifestContent);
